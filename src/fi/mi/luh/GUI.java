19b0367ea54ffd976fc7875e2ae11e8e200641eb
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2016-11-28 18:21:41 +0200

    Add javadoc

--- src/fi/mi/luh/GUI.java
+++ src/fi/mi/luh/GUI.java
@@ -3,11 +3,20 @@ package fi.mi.luh;
 /**
  * Created by M1k1tus on 18-Nov-16.
  *  @author Mikko Luhtasaari
+ *  @version 1.0, 18 Nov 2016
+ *  @since 1.0
+ *
+ *  Starting class for creating window.
  */
 public class GUI {
-    MyWindow ikkuna = new MyWindow();
+    MyWindow window = new MyWindow();
+
+    /**
+     * Makes the window to be shown.
+     *
+     */
     public GUI(){
-        ikkuna.setVisible(true);
+        window.setVisible(true);
     }
 
 }
