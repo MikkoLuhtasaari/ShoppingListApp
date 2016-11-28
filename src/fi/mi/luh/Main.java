0af9d768273d3acd5ecfb21d77c00d1b006855ed
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2016-11-28 18:21:28 +0200

    Add javadoc

--- src/fi/mi/luh/Main.java
+++ src/fi/mi/luh/Main.java
@@ -1,5 +1,12 @@
 package fi.mi.luh;
 
+/**
+ * @author Mikko Luhtasaari
+ * @since 1.0, 18 Nov 2016
+ * @version 1.0
+ *
+ * Starting point for program.
+ */
 public class Main {
 
     public static void main(String[] args) {
