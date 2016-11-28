f2fefe90fbe0b03502cebe045ba9fdcf78c027be
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2016-11-28 18:18:37 +0200

    Remove path

--- src/fi/mi/luh/Main.java
+++ src/fi/mi/luh/Main.java
@@ -1,13 +1,8 @@
 package fi.mi.luh;
 
-import java.net.URLDecoder;
-
 public class Main {
-    static final String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 
     public static void main(String[] args) {
-        //String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        //String decodedPath = URLDecoder.decode(path, "UTF-8");
         //CLI juttu = new CLI();
         //juttu.run();
 
