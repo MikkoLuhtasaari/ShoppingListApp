da58443bea95f9becf3fee768bec119b58d4746c
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2016-11-29 10:02:57 +0200

    Pass java lint. Javadoc ready.

--- src/fi/mi/luh/GUI.java
+++ src/fi/mi/luh/GUI.java
@@ -1,11 +1,11 @@
 package fi.mi.luh;
 
 /**
- * Created by M1k1tus on 18-Nov-16.
+ * Starting class for creating window.
+ *
  * @author Mikko Luhtasaari
  * @version 1.0, 18 Nov 2016
  * @since 1.0
- * Starting class for creating window.
  */
 public class GUI {
     MyWindow window = new MyWindow();
